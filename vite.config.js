85be6f7cdf66ce568daefaf6aa4aee0267a34cdc
jsonhc <json_hc@163.com>
2024-10-16 12:22:33 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  publicPath: '/zpei-library/',
   plugins: [
     vue(),
     vueDevTools(),
